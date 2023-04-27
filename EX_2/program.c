008689527736aa33267ec8ab4cfb1ff19c76e3e6
Sebastian <sebastian.nordby.bjornstad@wlcom.no>
2023-04-27 11:01:49 +0200

    Switching to linux machine

--- EX_2/program.c
+++ EX_2/program.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include "integer-utils.h"
+
+struct OPPGAVE2_TALL_METADATA {
+    int iIndex;
+    int iNumber;
+    bool bIsFibonacci;
+    bool bIsPrimeNumber;
+    bool bIsSquareNumber;
+    bool bIsCubeNumber;
+    bool bIsPerfectNumber;
+    bool bIsAbdundantNumber;
+    bool bIsDeficientNumber;
+    bool bIsOddNumber;
+} OPPGAVE2_TALL_METADATA;
 
 int main() {
+    struct OPPGAVE2_TALL_METADATA *d = malloc(sizeof(OPPGAVE2_TALL_METADATA));
 
+    d->bIsAbdundantNumber = 0;
 
+    free(d);
+    d = NULL;
 
     return 0;
 }
